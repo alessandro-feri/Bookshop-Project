69462f5afe7a5741d851aec832c0542cfdc76957
alessandro <alessandro.feri@stud.unifi.it>
2020-12-29 18:01:12 +0100

    added test for find a book given its title
    and removed useless tests

--- bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
@@ -3,7 +3,6 @@ package com.feri.alessandro.attsw.project.repositories;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Collection;
-import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,58 +25,13 @@ public class BookRepositoryTest {
 	private BookRepository bookRepository;
 	
 	@Test
-	public void testJpaMapping() {
-		Book savedBook = entityManager.persistFlushFind(new Book(null, "testBook", "testType", 10));
-		assertThat(savedBook.getTitle()).isEqualTo("testBook");
-		assertThat(savedBook.getType()).isEqualTo("testType");
-		assertThat(savedBook.getPrice()).isEqualTo(10);
-		assertThat(savedBook.getId()).isNotNull();
-		assertThat(savedBook.getId()).isGreaterThan(0);
+	public void test_findBookByTitle() {
+		Book bookToSave = new Book(null, "testTitle", "testType", 10);
+		Book bookSaved = entityManager.persistFlushFind(bookToSave);
 		
-		LoggerFactory.getLogger(BookRepositoryTest.class).info("Saved Book: " + savedBook.toString());
-	}
-	
-	@Test
-	public void test_findAllUsingSave() {
-		Book bookSaved = bookRepository.save(new Book(null, "testBook", "testType", 10));
-		Collection<Book> books = bookRepository.findAll();
-		assertThat(books).containsExactly(bookSaved);
-	}
-	
-	@Test
-	public void test_findAllUsingPersistFlushFind() {
-		Book bookSaved = entityManager.persistFlushFind(new Book(null, "testBook", "testType", 10));
-		Collection<Book> books = bookRepository.findAll();
-		assertThat(books).containsExactly(bookSaved);
-	}
-	
-	@Test
-	public void test_deleteUsingJPARepositoryMethod() {
-		Book bookSaved1 = entityManager.persistFlushFind(new Book(null, "testBook", "testType", 10));
-		Book bookSaved2 = entityManager.persistFlushFind(new Book(null, "testBook", "testType", 10));
-		assertThat(bookRepository.findAll()).containsExactly(bookSaved1, bookSaved2);
-		
-		bookRepository.delete(bookSaved1);
-		
-		Collection<Book> bookListAfterDelete = bookRepository.findAll();
-		
-		assertThat(bookListAfterDelete).containsExactly(bookSaved2);
+		Book bookFound = bookRepository.findByTitle("testTitle");
+		assertThat(bookFound).isSameAs(bookSaved);
 		
 	}
-	
-	@Test
-	public void test_deleteAllUsingJPARepositoryMethod() {
-		Book bookSaved1 = bookRepository.save(new Book(null, "testBook", "testType", 10));
-		Book bookSaved2 = bookRepository.save(new Book(null, "testBook", "testType", 10));
-		assertThat(bookRepository.findAll()).containsExactly(bookSaved1, bookSaved2);
 		
-		bookRepository.deleteAll();
-		
-		Collection<Book> bookListAfterDeleteAll = bookRepository.findAll();
-		
-		assertThat(bookListAfterDeleteAll).doesNotContain(bookSaved1, bookSaved2);
-		assertThat(bookListAfterDeleteAll).isEmpty();
-		
-	}
-	
 }

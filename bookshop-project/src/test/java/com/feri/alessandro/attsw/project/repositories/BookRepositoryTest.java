8b9604c1a3b4b5001259f2c544739a644fdd4ca0
alessandro <alessandro.feri@stud.unifi.it>
2020-12-29 12:31:40 +0100

    added test for the saved entity

--- bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.feri.alessandro.attsw.project.repositories;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -19,6 +22,16 @@ public class BookRepositoryTest {
 	@Test
 	public void testJpaMapping() {
 		Book savedBook = entityManager.persistFlushFind(new Book(null, "testBook", "testType", 10));
+		assertThat(savedBook.getTitle()).isEqualTo("testBook");
+		assertThat(savedBook.getType()).isEqualTo("testType");
+		assertThat(savedBook.getPrice()).isEqualTo(10);
+		assertThat(savedBook.getId()).isNotNull();
+		assertThat(savedBook.getId()).isGreaterThan(0);
+		assertThat(savedBook.getId()).isEqualTo(1);
+		
+		LoggerFactory.getLogger(BookRepositoryTest.class).info("Saved Book: " + savedBook.toString());
 	}
+	
+	
 	
 }

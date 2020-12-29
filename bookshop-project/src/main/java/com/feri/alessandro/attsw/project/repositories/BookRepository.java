40995e53bb18ba7365d4bfa25d7386abee348de5
alessandro <alessandro.feri@stud.unifi.it>
2020-12-29 18:06:13 +0100

    added findBooksByType() in book repository interface

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package com.feri.alessandro.attsw.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feri.alessandro.attsw.project.model.Book;
@@ -7,5 +9,7 @@ import com.feri.alessandro.attsw.project.model.Book;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	Book findByTitle(String string);
+
+	List<Book> findBooksByType(String string);
 
 }

89d20acb64489cbdfec26a367a0c3ec63a1d9261
alessandro <alessandro.feri@stud.unifi.it>
2020-12-29 18:05:34 +0100

    added test for find books with the same type

--- bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/BookRepositoryTest.java
@@ -1,6 +1,9 @@
 package com.feri.alessandro.attsw.project.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,22 @@ public class BookRepositoryTest {
 		Book bookFound = bookRepository.findByTitle("testTitle");
 		assertThat(bookFound).isSameAs(bookSaved);
 		
+	}
+	
+	@Test
+	public void test_findBooksByType() {
+		Book testBook1 = new Book(null, "testTitle1", "type1", 10);
+		Book testBook2 = new Book(null, "testTitle2", "type2", 10);
+		Book testBook3 = new Book(null, "testTitle3", "type1", 10);
+		
+		entityManager.persistFlushFind(testBook1);
+		entityManager.persistFlushFind(testBook2);
+		entityManager.persistFlushFind(testBook3);
+		
+		List<Book> books = bookRepository.findBooksByType("type1");
+		
+		assertThat(books).containsExactly(testBook1, testBook3);
+		assertThat(books).doesNotContain(testBook2);
 	}
 		
 }

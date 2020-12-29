bf069c6adc8f9db813c6bf190af28af3b27cbb02
alessandro <alessandro.feri@stud.unifi.it>
2020-12-29 13:24:05 +0100

    added BookRepository Interface

---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.feri.alessandro.attsw.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.feri.alessandro.attsw.project.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}

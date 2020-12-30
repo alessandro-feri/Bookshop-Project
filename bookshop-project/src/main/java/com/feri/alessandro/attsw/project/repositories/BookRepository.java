2d2676261dc8bfa93f20545a28d3878e672cd537
alessandro <alessandro.feri@stud.unifi.it>
2020-12-30 14:29:39 +0100

    added query for finAllBooksWhosePriceIsWithinAnInterval() in book repository interface

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/BookRepository.java
@@ -3,6 +3,8 @@ package com.feri.alessandro.attsw.project.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.feri.alessandro.attsw.project.model.Book;
 
@@ -16,4 +18,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
 	Book findByTitleAndPrice(String title, int price);
 
+	@Query("Select b from Book b where b.price > :min and b.price < :max")
+	List<Book> findAllBooksWhosePriceIsWithinAnInterval(@Param("min") int min, @Param("max") int max);
+	
 }

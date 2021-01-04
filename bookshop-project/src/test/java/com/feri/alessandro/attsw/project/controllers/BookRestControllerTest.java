66085a8e6aa950e104dcc895f06674517eed9d81
alessandro <alessandro.feri@stud.unifi.it>
2021-01-04 21:06:01 +0100

    first failing test for GET end-point
    test getAllbooks (empty and not empty)

---
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/controllers/BookRestControllerTest.java
@@ -0,0 +1,72 @@
+package com.feri.alessandro.attsw.project.controllers;
+
+import static java.util.Arrays.asList;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+
+import com.feri.alessandro.attsw.project.model.Book;
+import com.feri.alessandro.attsw.project.services.BookService;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookRestControllerTest {
+
+	@Mock
+	private BookService bookService;
+	
+	@InjectMocks
+	private BookRestController bookRestController;
+	
+	@Before
+	public void setUp() {
+		RestAssuredMockMvc.standaloneSetup(bookRestController);
+	}
+	
+	@Test
+	public void test_allBooksEmpty() {
+		when(bookService.getAllBooks()).thenReturn(Collections.emptyList());
+		
+		given().
+		when().
+			get("/api/books").
+		then().
+			statusCode(200).
+			assertThat().
+				body(is(equalTo("[]")));
+	}
+	
+	@Test
+	public void test_allBooksNotEmpty() {
+		Book testBook1 = new Book(1L, "Il ritratto di Dorian Gray", "romanzo", 7);
+		Book testBook2 = new Book(2L, "Harry Potter e la pietra filosofale", "romanzo", 9);
+		when(bookService.getAllBooks()).thenReturn(asList(testBook1, testBook2));
+		
+		given().
+		contentType(MediaType.APPLICATION_JSON_VALUE).
+		when().
+			get("/api/books").
+		then().
+			statusCode(200).
+			assertThat().
+				body("id[0]", equalTo(1),
+					 "title[0]", equalTo("Il ritratto di Dorian Gray"),
+					 "type[0]", equalTo("romanzo"),
+					 "price[0]", equalTo(7),
+					 "id[1]", equalTo(2),
+					 "title[1]", equalTo("Harry Potter e la pietra filosofale"),
+					 "type[1]", equalTo("romanzo"),
+					 "price[1]", equalTo(9));
+	}
+}

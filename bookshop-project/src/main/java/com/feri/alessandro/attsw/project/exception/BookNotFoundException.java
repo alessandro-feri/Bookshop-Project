9b7eb78d4c827e206c69c84e670f6d526695e24e
alessandro <alessandro.feri@stud.unifi.it>
2021-01-07 17:22:48 +0100

    added constructor in BookNotFoundException class

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/BookNotFoundException.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/BookNotFoundException.java
@@ -7,4 +7,10 @@ public class BookNotFoundException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	public BookNotFoundException(String message) {
+		super(message);
+	}
+
+
+	
 }

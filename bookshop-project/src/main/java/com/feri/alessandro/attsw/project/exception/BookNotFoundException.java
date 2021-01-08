fab6b2571b53fe39a6b732d0ae0a4195d0f25c5e
alessandro <alessandro.feri@stud.unifi.it>
2021-01-08 16:11:55 +0100

    created BookNotFoundException in .exception package

---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/BookNotFoundException.java
@@ -0,0 +1,10 @@
+package com.feri.alessandro.attsw.project.exception;
+
+public class BookNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}

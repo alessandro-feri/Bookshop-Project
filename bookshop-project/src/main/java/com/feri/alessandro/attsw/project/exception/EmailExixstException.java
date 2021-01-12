671ea1a13fa06e91b8e0a2eaf7afd5a44135e368
alessandro <alessandro.feri@stud.unifi.it>
2021-01-12 18:50:06 +0100

    created EmailExist and UsernameExist exceptions

---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/EmailExixstException.java
@@ -0,0 +1,14 @@
+package com.feri.alessandro.attsw.project.exception;
+
+public class EmailExixstException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public EmailExixstException(String message) {
+		super(message);
+	}
+
+}
---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/UsernameExistException.java
@@ -0,0 +1,14 @@
+package com.feri.alessandro.attsw.project.exception;
+
+public class UsernameExistException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public UsernameExistException(String message) {
+		super(message);
+	}
+
+}

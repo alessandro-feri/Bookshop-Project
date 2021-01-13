bb790f7094cab5ee3392050c79e01b05e73dfd95
alessandro <alessandro.feri@stud.unifi.it>
2021-01-13 19:59:28 +0100

    Let's start with User Repository testing: findAll()

---
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/repositories/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.feri.alessandro.attsw.project.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.feri.alessandro.attsw.project.model.User;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testJpaMapping() {
+		User savedUser = entityManager.persistFlushFind(new User(null, "email", "username", "password"));
+		assertThat(savedUser.getEmail()).isEqualTo("email");
+		assertThat(savedUser.getUsername()).isEqualTo("username");
+		assertThat(savedUser.getPassword()).isEqualTo("password");
+		assertThat(savedUser.getId()).isNotNull();
+		assertThat(savedUser.getId()).isGreaterThan(0);
+		
+		LoggerFactory.getLogger(UserRepositoryTest.class).info("Saved user " + savedUser.toString());
+	}
+	
+	@Test
+	public void test_findAllWithEmptyDatabase() {
+		List<User> users = userRepository.findAll();
+		assertThat(users).isEmpty();
+	}
+	
+	@Test
+	public void test_findAllUsingSave() {
+		User userSaved = userRepository.save(new User(1L, "email", "username", "password"));
+		List<User> users = userRepository.findAll();
+		assertThat(users).containsExactly(userSaved);		
+	}
+	
+	
+	
+
+}

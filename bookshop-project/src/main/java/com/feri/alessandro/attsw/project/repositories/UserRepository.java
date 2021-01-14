ed2ceb3fbbcfbbf7d373165ec3ce75681760f4ae
alessandro <alessandro.feri@stud.unifi.it>
2021-01-14 11:23:24 +0100

    create findByUsername in UserRepository

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/UserRepository.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.feri.alessandro.attsw.project.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.feri.alessandro.attsw.project.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
 
 }

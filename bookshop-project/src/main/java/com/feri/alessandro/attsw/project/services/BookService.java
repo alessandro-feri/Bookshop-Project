d4959f044b47693f18a95d8c5c5d4a142d567756
alessandro <alessandro.feri@stud.unifi.it>
2021-01-27 17:41:49 +0100

    implementation of home view with book list
    and fake implementation in service

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/services/BookService.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/services/BookService.java
@@ -1,8 +1,18 @@
 package com.feri.alessandro.attsw.project.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.feri.alessandro.attsw.project.model.Book;
 
 @Service
 public class BookService {
+
+	private static final String TEMPORARY_IMPLEMENTATION = "temporary_implementation";
+
+	public List<Book> getAllBooks() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
 
 }
--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/web/BookWebController.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/web/BookWebController.java
@@ -1,15 +1,28 @@
 package com.feri.alessandro.attsw.project.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.feri.alessandro.attsw.project.model.Book;
+import com.feri.alessandro.attsw.project.services.BookService;
 
 
 @Controller
 public class BookWebController {
 	
+	@Autowired
+	private BookService bookService;
+	
 	@GetMapping("/")
 	public String getIndex(Model model) {
+		List<Book> allBooks = bookService.getAllBooks();
+		model.addAttribute("books", allBooks);
+		model.addAttribute("message", allBooks.isEmpty() ? "No books!" : "");
+		
 		return "index";
 	}
 	

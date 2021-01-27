3958393466b85600b54101d3d160c3535950e457
alessandro <alessandro.feri@stud.unifi.it>
2021-01-27 18:13:08 +0100

    added BookNotFound exception and WebExceptionHandler

---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/exception/BookWebExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.feri.alessandro.attsw.project.exception;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class BookWebExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(value = BookNotFoundException.class)
+	public String handleBookNotFoundException(Model model, HttpServletResponse response) {
+		
+		model.addAttribute("message", "Book not found!");
+		response.setStatus(404);
+		
+		return "bookNotFound";
+	}
+
+}

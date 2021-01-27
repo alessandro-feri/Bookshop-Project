425e770cd69678bf9460a348d982dc3d4d6fb387
alessandro <alessandro.feri@stud.unifi.it>
2021-01-27 18:16:05 +0100

    implementation of GET mapping for edit

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/services/BookService.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/services/BookService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.feri.alessandro.attsw.project.exception.BookNotFoundException;
 import com.feri.alessandro.attsw.project.model.Book;
 
 @Service
@@ -12,6 +13,10 @@ public class BookService {
 	private static final String TEMPORARY_IMPLEMENTATION = "temporary_implementation";
 
 	public List<Book> getAllBooks() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+	public Book getBookById(long id) throws BookNotFoundException {
 		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
 	}
 
--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/web/BookWebController.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/web/BookWebController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.feri.alessandro.attsw.project.exception.BookNotFoundException;
 import com.feri.alessandro.attsw.project.model.Book;
 import com.feri.alessandro.attsw.project.services.BookService;
 
@@ -24,6 +26,15 @@ public class BookWebController {
 		model.addAttribute("message", allBooks.isEmpty() ? "No books!" : "");
 		
 		return "index";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editBookById(@PathVariable Long id, Model model) throws BookNotFoundException {
+		Book book = bookService.getBookById(id);
+		model.addAttribute("book", book);
+		model.addAttribute("message", "");
+			
+		return "edit";
 	}
 	
 }

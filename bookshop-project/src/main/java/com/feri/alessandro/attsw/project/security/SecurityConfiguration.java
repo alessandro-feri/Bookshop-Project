e4b29df41f095cc9ca5c4f0567b038ba4e6ff8e9
alessandro <alessandro.feri@stud.unifi.it>
2021-02-09 14:04:22 +0100

    added security configuration class

---
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.feri.alessandro.attsw.project.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.feri.alessandro.attsw.project.security")
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationSuccessHandler getSuccessHandler() {
+		return new UserAuthenticationSuccessHandler();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/registration").permitAll()
+			.antMatchers("/", "/new", "/save", "/edit/**", "/search", "/delete", "/deleteAll").authenticated()
+			.and().csrf().disable()
+			.formLogin().successHandler(getSuccessHandler())
+			.loginPage("/login").failureUrl("/login?error=true")
+			.usernameParameter("username")
+			.passwordParameter("password")
+			.and().logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/login").and().exceptionHandling();
+	}
+}

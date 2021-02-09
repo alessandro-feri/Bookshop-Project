ef53e83f6dff315b90cd086e7e9b51951caacfee
alessandro <alessandro.feri@stud.unifi.it>
2021-02-09 16:23:25 +0100

    added password encoder in security configuration

--- bookshop-project/src/main/java/com/feri/alessandro/attsw/project/security/SecurityConfiguration.java
+++ bookshop-project/src/main/java/com/feri/alessandro/attsw/project/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -19,6 +20,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		return new UserAuthenticationSuccessHandler();
 	}
 	
+	@Bean
+	public BCryptPasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -29,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and().csrf().disable()
 			.formLogin().successHandler(getSuccessHandler())
 			.loginPage("/login").failureUrl("/login?error=true")
-			.usernameParameter("username")
+			.usernameParameter("email")
 			.passwordParameter("password")
 			.and().logout()
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))

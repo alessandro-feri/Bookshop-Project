6c7cf76dcf64f355e051eedbca3fa0092954b67d
alessandro <alessandro.feri@stud.unifi.it>
2021-02-14 20:34:16 +0100

    added test for home view

--- bookshop-project/src/test/java/com/feri/alessandro/attsw/project/web/BookshopWebViewsHtmlUnitTest.java
+++ bookshop-project/src/test/java/com/feri/alessandro/attsw/project/web/BookshopWebViewsHtmlUnitTest.java
@@ -3,21 +3,28 @@ package com.feri.alessandro.attsw.project.web;
 import static com.gargoylesoftware.htmlunit.WebAssert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
+import static java.util.Arrays.asList;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.feri.alessandro.attsw.project.exception.EmailExistException;
 import com.feri.alessandro.attsw.project.exception.UsernameExistException;
+import com.feri.alessandro.attsw.project.model.Book;
 import com.feri.alessandro.attsw.project.services.BookService;
 import com.feri.alessandro.attsw.project.services.UserService;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = BookshopWebController.class)
@@ -142,6 +149,68 @@ public class BookshopWebViewsHtmlUnitTest {
 				"You have been registered successfully!"));
 		assertLinkPresentWithText(result, "Login Page");
 		
+	}
+	
+	
+	@Test
+	@WithMockUser
+	public void test_HomePageTitle() throws Exception {
+		HtmlPage page = webClient.getPage("/");
+		assertThat(page.getTitleText()).isEqualTo("Bookshop");
+	}
+
+	@Test
+	@WithMockUser
+	public void test_HomePageStructure() throws Exception {
+		HtmlPage page = webClient.getPage("/");
+		
+		assertThat(page.getAnchorByText("Insert").getHrefAttribute()).isEqualTo("/new");
+		assertThat(page.getBody().getTextContent()).contains("There are no books");
+		assertLinkNotPresentWithText(page, "Edit");
+		assertLinkNotPresentWithText(page, "Delete");
+		assertTextPresent(page, "Search");
+		assertFormPresent(page, "search_form");
+		assertInputPresent(page, "title_searched");
+		assertElementPresent(page, "btn_search");
+		assertFormPresent(page, "deleteAll");
+		assertFormPresent(page, "logout");
+
+	}	
+	
+	@Test
+	@WithMockUser
+	public void test_homePage_WithNoBooks() throws Exception {
+		when(bookService.getAllBooks()).thenReturn(Collections.emptyList());
+		
+		HtmlPage page = webClient.getPage("/");
+		
+		assertThat(page.getBody().getTextContent()).contains("There are no books");
+	}
+	
+	@Test
+	@WithMockUser
+	public void test_homePageWithBooks_shouldShowThemInATable() throws Exception {
+		List<Book> books = asList(
+				new Book(1L, "title1", "type1", 10), new Book(2L, "title2", "type2", 15));
+		
+		when(bookService.getAllBooks()).thenReturn(books);
+		
+		HtmlPage page = webClient.getPage("/");
+		
+		assertThat(page.getBody().getTextContent()).doesNotContain("No books");
+		HtmlTable table = page.getHtmlElementById("book_table");
+		
+		assertThat(table.asText()).isEqualTo(
+				"ID	Title	Type	Price\n" + 
+				"1	title1	type1	10	Edit	Delete\n" + 
+				"2	title2	type2	15	Edit	Delete"
+				
+			);	
+		
+		page.getAnchorByHref("/edit/1");
+		page.getAnchorByHref("/edit/2");
+		page.getAnchorByHref("/delete?id=1");
+		page.getAnchorByHref("/delete?id=2");
 	}
 	
 }

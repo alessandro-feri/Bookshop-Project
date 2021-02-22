0fa4d208dd43fd89c99bf6fb58711ec36b018a1d
alessandro <alessandro.feri@stud.unifi.it>
2021-02-22 17:29:18 +0100

    added test for create new User

--- bookshop-project/src/it/java/com/feri/alessandro/attsw/project/BookshopWebControllerIT.java
+++ bookshop-project/src/it/java/com/feri/alessandro/attsw/project/BookshopWebControllerIT.java
@@ -1,5 +1,7 @@
 package com.feri.alessandro.attsw.project;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
@@ -11,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.feri.alessandro.attsw.project.model.User;
 import com.feri.alessandro.attsw.project.repositories.BookRepository;
 import com.feri.alessandro.attsw.project.repositories.UserRepository;
 
@@ -26,6 +30,9 @@ public class BookshopWebControllerIT {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
 	private BookRepository bookRepository;
@@ -72,6 +79,25 @@ public class BookshopWebControllerIT {
 		ModelAndViewAssert.assertViewName(
 				mvc.perform(get("/registration")).
 					andReturn().getModelAndView(), "registration");
+	}
+	
+	@Test
+	public void test_createNewUser_shouldReturnResultPage() throws Exception {
+		ModelAndViewAssert.assertViewName(mvc.perform(post("/saveUser")
+				.param("id", "1")
+				.param("email", "email@gmail")
+				.param("username", "test")
+				.param("password", "password")).andReturn().getModelAndView(), "registrationResult");
+		
+		assertEquals(userRepository.findAll().size(), 1);
+		
+		User saved = userRepository.findAll().get(0);
+		
+		assertEquals(saved.getId(), "1");
+		assertEquals(saved.getEmail(), "email@gmail");
+		assertEquals(saved.getUsername(), "test");
+		assertThat(bCryptPasswordEncoder.matches("password", saved.getPassword()));
+		
 	}
 	
 	
